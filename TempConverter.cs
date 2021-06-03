ebd8d786b7999d2b6453169b9584fa4e3ccbf8a6
infopek <10saero01@gmail.com>
2021-06-03 03:08:27 +0200

    Initial commit

---
+++ TempConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TempConverter
+{
+    class Program
+    {
+        // Convert temperature (°C) to K or °F
+        static void Main(string[] args)
+        {
+            double cTemp = 32;
+
+            Console.WriteLine(ToFahrenheit(cTemp));
+            Console.WriteLine(ToKelvin(cTemp));
+
+            Console.ReadLine();
+        }
+
+        // Convert Celsius to Kelvin
+        static double ToKelvin(double temp)
+        {
+            return temp + 273.15;
+        }
+
+        // Convert Celsius to Fahrenheit
+        static double ToFahrenheit(double temp)
+        {
+            return (temp * 9 / 5) + 32;
+        }
+    }
+}
